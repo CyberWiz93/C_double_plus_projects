f4fdfbaff769839827503ff3015e216f05b391c7
Tony <103059603+CyberWiz93@users.noreply.github.com>
2025-02-18 21:57:13 -0500

    Add files via upload

---
+++ convert/converting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  
+  double tempf = 21;
+  double tempc = -6.1;
+  
+  tempc = (tempf-32)/1.8;
+  
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n"; 
+  
+  
+}
---
+++ interger/learning int.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+int tip = 0;
+
+std::cout << "Please enter tip: ";
+std::cin >> tip;
+
+std::cout << "You paid " << tip << " dollars.\n";
+
+
+
+
+
+}
